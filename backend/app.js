908456d0b6968e78f015a507e76f18a10843a225
misha1204 <Mikheil.kvashali@gmail.com>
2021-10-27 16:24:25 +0400

    Update backend

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://gio:charkviani1616@cluster0.hkl9m.mongodb.net/ngrxlearning?retryWrites=true&w=majority"
-).then(()=>{
-  console.log('Connected to database!')
-}).catch(()=>{
-  console.log("Connection failed!")
-})
+mongoose
+  .connect(
+    "mongodb+srv://Misha1204:Misha246813579@cluster0.3vdof.mongodb.net/NgRxLearning?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 
 const Person = require("./models/person");
 const Country = require("./models/country");
@@ -46,51 +49,58 @@ app.get("/api/persons", async (req, res, next) => {
   }
 });
 
-app.post('/api/persons', async (req, res, next) => {
+app.post("/api/persons", async (req, res, next) => {
   try {
-    const person = new Person({name: req.body.name, surname: req.body.surname, age: req.body.age});
+    const person = new Person({
+      name: req.body.name,
+      surname: req.body.surname,
+      age: req.body.age,
+    });
 
     const personSaved = await person.save();
 
-    if(!personSaved) {
-      return res.status(400).send({error: "Not saved!"})
+    if (!personSaved) {
+      return res.status(400).send({ error: "Not saved!" });
     }
 
-    res.send({message: "User added syccessfully", person: personSaved})
+    res.send({ message: "User added syccessfully", person: personSaved });
   } catch (e) {
-    res.status(500).send({error: "Server error!"})
+    res.status(500).send({ error: "Server error!" });
   }
-})
+});
 
 app.delete("/api/persons/:id", async (req, res, next) => {
   try {
-    const personDeleted = await Person.findByIdAndDelete(req.params.id)
+    const personDeleted = await Person.findByIdAndDelete(req.params.id);
 
-    if(!personDeleted) {
-      return res.status(404).send({error: "Error, Person not found!"})
+    if (!personDeleted) {
+      return res.status(404).send({ error: "Error, Person not found!" });
     }
 
-    res.send({message: "User deleted successfully", deletedUser: personDeleted})
+    res.send({
+      message: "User deleted successfully",
+      deletedUser: personDeleted,
+    });
   } catch (error) {
-    res.status(500).send({error: "Server error"})
+    res.status(500).send({ error: "Server error" });
   }
-})
+});
 
 app.patch("/api/persons/:id", async (req, res) => {
   try {
     const updatedUser = req.body;
 
-    const person = await Person.findByIdAndUpdate(req.params.id, updatedUser)
+    const person = await Person.findByIdAndUpdate(req.params.id, updatedUser);
 
-    if(!person) {
-      res.status(404).send({error: "Person not found!"})
+    if (!person) {
+      res.status(404).send({ error: "Person not found!" });
     }
 
-    res.send(person)
+    res.send(person);
   } catch (error) {
-    res.status(500).send({error: "Server Error!"})
+    res.status(500).send({ error: "Server Error!" });
   }
-})
+});
 
 //countries
 
@@ -110,51 +120,59 @@ app.get("/api/countries", async (req, res, next) => {
   }
 });
 
-app.post('/api/countries', async (req, res, next) => {
+app.post("/api/countries", async (req, res, next) => {
   try {
-    const country = new Country({name: req.body.name, capital: req.body.capital});
+    const country = new Country({
+      name: req.body.name,
+      capital: req.body.capital,
+    });
 
     const countrySaved = await country.save();
 
-    if(!countrySaved) {
-      return res.status(400).send({error: "Not saved!"})
+    if (!countrySaved) {
+      return res.status(400).send({ error: "Not saved!" });
     }
 
-    res.send({message: "User added syccessfully", country: countrySaved})
+    res.send({ message: "User added syccessfully", country: countrySaved });
   } catch (e) {
-    res.status(500).send({error: "Server error!"})
+    res.status(500).send({ error: "Server error!" });
   }
-})
+});
 
 app.delete("/api/countries/:id", async (req, res, next) => {
   try {
-    const countryDeleted = await Country.findByIdAndDelete(req.params.id)
+    const countryDeleted = await Country.findByIdAndDelete(req.params.id);
 
-    if(!countryDeleted) {
-      return res.status(404).send({error: "Error, country not found!"})
+    if (!countryDeleted) {
+      return res.status(404).send({ error: "Error, country not found!" });
     }
 
-    res.send({message: "User deleted successfully", deletedCountry: countryDeleted})
+    res.send({
+      message: "User deleted successfully",
+      deletedCountry: countryDeleted,
+    });
   } catch (error) {
-    res.status(500).send({error: "Server error"})
+    res.status(500).send({ error: "Server error" });
   }
-})
+});
 
 app.patch("/api/countries/:id", async (req, res) => {
   try {
     const updatedCountry = req.body;
 
-    const country = await Country.findByIdAndUpdate(req.params.id, updatedCountry)
+    const country = await Country.findByIdAndUpdate(
+      req.params.id,
+      updatedCountry
+    );
 
-    if(!country) {
-      res.status(404).send({error: "country not found!"})
+    if (!country) {
+      res.status(404).send({ error: "country not found!" });
     }
 
-    res.send(country)
+    res.send(country);
   } catch (error) {
-    res.status(500).send({error: "Server Error!"})
+    res.status(500).send({ error: "Server Error!" });
   }
-})
-
+});
 
 module.exports = app;
